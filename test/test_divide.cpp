749fcddea56f0709b8d42b0a805b2bd8c1ca69a8
a01177702 <132400887+a01177702@users.noreply.github.com>
2025-05-19 20:35:09 -0600

    error pruebas 3

--- test/test_divide.cpp
+++ test/test_divide.cpp
@@ -10,6 +10,13 @@ EXPECT_EQ(result, 3);
 
 TEST(DivTest, HandlesZeroInput)
 {
+int result = divide(0, 0);
+
+EXPECT_EQ(result, 0);
+}
+
+TEST(DivTest, HandlesNesgativeInput)
+{
 int result = divide(-6, -3);
 
 EXPECT_EQ(result, 2);
